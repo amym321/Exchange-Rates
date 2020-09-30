eccaf522d2350f995a254844efd49c01eeac2c1f
amym321 <66137093+amym321@users.noreply.github.com>
2020-09-29 20:57:53 -0500

    seperate currency options list

--- src/components/CurrencyForm/CurrencyOptions.js
+++ src/components/CurrencyForm/CurrencyOptions.js
@@ -1,30 +1,60 @@
-import React from 'react';
 
 
-class CurrencyOptions extends React.Component {
-    render() { 
-        return (
-            <select required
-                className="pa2 input-reset ba bg-transparent hover-bg-white w-100 black" 
-                type="text" 
-                name="initialCurrency"  
-                id="initialCurrency"
-                onChange={this.onInitialCurrencyChange}
-                // value={InitialCurrency}
-                >
-                <option>~Please select currency~</option>
-                <option id="USD" id="United States of America">United States Dollar</option>
-                <option id="EUR" id="France">Euro (used in France)</option>
-                <option id="EUR" id="Austria">Euro (used in Austria)</option>
-                <option id="EUR" id="Germany">Euro (used in Germany)</option>
-                <option id="GBP" id="Britain">British Pound Sterling</option>
-                <option id="MXN" id="Mexico">Mexican Peso</option>
-                <option id="AED" id="United Arab Emirates">United Arab Emirates Dirham</option>
-            </select>
-        )
-    }
-}
+// const CurrencyOptions = ({ options }) => {
+// class CurrencyOptions extends React.Component {
+//     render() {
+//         return (
+//             <div></div>
+export const options = [
+                { value: 'USD', label: 'United States Dollar' },
+                { value: 'AUD', label: 'Australian Dollar' },
+                { value: 'BRL', label: 'Brazilian Real' },
+                { value: 'GBP', label: 'British Pound Sterling' },
+                { value: 'BLG', label: 'Bulgarian Lev' },
+                { value: 'CAD', label: 'Canadian Dollar' },
+                { value: 'CLP', label: 'Chilean Peso' },
+                { value: 'CNY', label: 'Chinese Yuan' },            
+                { value: 'COP', label: 'Colombian Peso' },
+                { value: 'CZK', label: 'Czech Republic Koruna' },
+                { value: 'DKK', label: 'Danish Krone' },
+                { value: 'DOP', label: 'Dominican Peso' },
+                { value: 'EGP', label: 'Egyptian Pound' },
+                { value: 'EUR', label: 'Euro (used in France)' },
+                { value: 'EUR', label: 'Euro (used in Germany)' },
+                { value: 'EUR', label: 'Euro (used in Austria)' },
+                { value: 'EUR', label: 'Euro (used in Belgium)' },
+                { value: 'EUR', label: 'Euro (used in Greece)' },
+                { value: 'EUR', label: 'Euro (used in Ireland)' },
+                { value: 'EUR', label: 'Euro (used in Italy)' },
+                { value: 'EUR', label: 'Euro (used in Netherlands)' },
+                { value: 'EUR', label: 'Euro (used in Portugal)' },
+                { value: 'EUR', label: 'Euro (used in Spain)' },    
+                { value: 'GTQ', label: 'Guatemalan Quetzal' },
+                { value: 'HKD', label: 'Hong Kong Dollar' },
+                { value: 'HUF', label: 'Hungarian Forint' },
+                { value: 'IDR', label: 'Indonesian Rupiah' },
+                { value: 'ILS', label: 'Israeli New Sheqel"' },
+                { value: 'INR', label: 'Indian Rupee' },
+                { value: 'ISK', label: 'Icelandic Króna' },
+                { value: 'JPY', label: 'Japanese Yen' },
+                { value: 'KRW', label: 'South Korean Won' },
+                { value: 'MYR', label: 'Malaysian Ringgit"' },      
+                { value: 'MXN', label: 'Mexican Peso' },
+                { value: 'NOK', label: 'Norwegian Krone' },
+                { value: 'NZD', label: 'New Zealand Dollar' },
+                { value: 'PHP', label: 'Philippine Peso' },
+                { value: 'PLN', label: 'Polish Zloty' },
+                { value: 'RUB', label: 'Russian Ruble' },
+                { value: 'SAR', label: 'Saudi Riyal' },
+                { value: 'SEK', label: 'Swedish Krona' },
+                { value: 'CHF', label: 'Swiss Franc' },
+                { value: 'THB', label: 'Thai Baht' },
+                { value: 'TRY', label: 'Turkish Lira' },
+                { value: 'ZAR', label: 'South African Rand' },
+                { value: 'AED', label: 'United Arab Emirates Dirham' } 
+            ];
+//         )
+//     }
+// }
 
-
-
-export default CurrencyOptions;
+// export default CurrencyOptions;
