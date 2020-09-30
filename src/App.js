f033ac45c8abbb01d40ffd79ebb928941b3a6153
amym321 <66137093+amym321@users.noreply.github.com>
2020-09-29 20:04:48 -0500

    styling

--- src/App.js
+++ src/App.js
@@ -17,28 +17,37 @@ import FlagA from './images/flagUS.jpg';
 import FlagB from './images/flagFrance.jpg';
 import Money from './images/papercurrency1.mp4';
 
-
-
-// from face finder app.js
 const initialState = {
 	input: '',
 	imageUrl: '',
-	box: {},
-    // route: 'signin',
     route: 'home',
-	isSignedIn: false,
+    isSignedIn: false,
+    amountIn: 0,
+    amountOut: 0,
+    currencyFrom: '',
+    currencyTo: '',
+    countryA: '',
+    countryB: '',
+    languageA: '',
+    languageB: '',
+    populationA: '',
+    populationB: '',
+    capitalA: '',
+    capitalB: '',
+    regionA: '',
+    regionB: '',
+    subregionA: '',
+    subregionB: '',
 	user: {
 		id: '',
 		name: '',
-		email: '',
-		entries: 0,
-		joined: ''
+        email: ''
 	}
 }
 
 
 // export default function Landing() {
-class Landing extends Component {
+class App extends Component {
 	constructor() {
 		super();
 		this.state = initialState;
@@ -49,7 +58,7 @@ class Landing extends Component {
 			id: data.id,
 			name: data.name,
 			email: data.email
-		}})
+        }})
 	}
 
     onInputChange = (event) => {
@@ -77,9 +86,8 @@ class Landing extends Component {
 
 
     render() {
-        const { isSignedIn, route } = this.state;
-        return (
-      
+        const { isSignedIn, route, amountIn, amountOut, currencyFrom, currencyTo } = this.state;
+        return (      
         <div>
         
             {/* <Navbar transparent /> */}
@@ -92,7 +100,7 @@ class Landing extends Component {
                         style={{
                         backgroundImage: `url(${Background})`
                         }}>
-x                    <span id="blackOverlay" className="w-full h-full absolute opacity-50 bg-black"></span>
+                    <span id="blackOverlay" className="w-full h-full absolute opacity-50 bg-black"></span>
                     </div>
                     <div className="container relative mx-auto">
                         <div className="items-center flex flex-wrap">
@@ -158,7 +166,7 @@ x                    <span id="blackOverlay" className="w-full h-full absolute o
                                 <p className="mt-2 mb-4 text-gray-600">
                                     Free conversion of one currency into another currency for your business or personal needs.                             
                                 </p>
-                                <img className="mt-2 mb-4 text-gray-600 rounded-lg" src={Rates} alt="image of project"></img>
+                                <img className="mt-2 mb-4 text-gray-600 rounded-lg" src={Rates} alt="project"></img>
                             </div>
                             </div>
                         </div>
@@ -192,7 +200,7 @@ x                    <span id="blackOverlay" className="w-full h-full absolute o
                                 </h6>
                                 <p className="mt-2 mb-4 text-gray-600">Get information on the countries of the currencies as well.
                                 Rates are updated hourly.</p>
-                                <img className="mt-2 mb-4 text-gray-600" src={Mockup} alt="image of project"></img>
+                                <img className="mt-2 mb-4 text-gray-600" src={Mockup} alt="project"></img>
                             </div>
                             </div>
                         </div>
@@ -201,16 +209,15 @@ x                    <span id="blackOverlay" className="w-full h-full absolute o
 
                     <div className="relative pt-32 pb-32 flex content-center items-center justify-center mt-24">
                         <div className="absolute w-full h-full bg-center bg-cover">
-                            <video fluid={false} width={"100%"} height={"100%"} overflow-hidden loop muted autoPlay
+                            <video width={"100%"} height={"100%"} overflow-hidden loop muted autoPlay
                                 style={{objectFit: "cover"}}>
-                                <source fluid={false}  src={Money} type="video/mp4" />
+                                <source fluid="false"  src={Money} type="video/mp4" />
                             </video>
                         </div>
                     </div>
 
                 </section>
                 
-
                 : <section className=" bg-gray-300 -mt-24">
 
                     <div className="container mx-auto px-4">
@@ -224,7 +231,15 @@ x                    <span id="blackOverlay" className="w-full h-full absolute o
                                 <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
                                     <div className="px-4 py-5 flex-auto">
                                         <div className="mt-2 mb-4 text-gray-600">
-                                            <CurrencyForm />
+                                            <CurrencyForm 
+                                                onInputChange={this.onInputChange}  
+                                                onButtonSubmit={this.onButtonSubmit}
+                                                amountIn={amountIn} 
+                                                amountOut={amountOut} 
+                                                currencyFrom={currencyFrom} 
+                                                currencyTo={currencyTo}
+                                            />
+                                            {console.log('21', this.state.currencyFrom)}                                            
                                         </div>
                                     </div>
                                 </div>
@@ -232,83 +247,91 @@ x                    <span id="blackOverlay" className="w-full h-full absolute o
                         </div>
                         
 
-                        <div className="flex flex-wrap items-center mt-24">
 
-                            <div className="w-full md:w-8/12 px-4 mr-auto ml-auto  text-center" data-aos="zoom-in-up" data-aos-delay="0">
-                                {/* <div className="text-gray-600 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-gray-100">
-                                <i className="fas fa-user-friends text-xl"></i>
-                                </div> */}
-                                <h3 className="text-3xl mb-2 font-semibold leading-normal">
-                                Your Currency Conversion                                
-                                </h3>
-                                <p className="text-lg font-light leading-relaxed mt-4 mb-4 text-gray-700">
-                                "symbolA" "AmountIn" "from currency" from "CountryName A" equals "symbolB" 
-                                "AmountOut" "To currency" from "CountryName B". 
-                                {/* £20 Pound Sterling from the United Kingdom equals €21.85 Euro from France.                                 */}
-                                </p>
-                            </div>
-                        </div>
 
-        
-                        <div class="flex flex-wrap items-center ">       
-                            <div class="w-full md:w-4/12 px-4 mr-auto ml-auto mt-24" data-aos="zoom-in-right">
-                                <h3 class="text-3xl mb-2 font-semibold leading-normal">
-                                "Country A"
-                                {/* France */}
-                                </h3>
-                                <p class="text-lg font-light leading-relaxed mt-4 mb-4 text-gray-700">
-                                "CountryName A" uses "from currency" as currency and speaks "languageA". 
-                                {/* France uses Euro as currency and speaks French. */}
-                                </p>
-                                <p class="text-lg font-light leading-relaxed mt-0 mb-4 text-gray-700">
-                                "CountryName A" is located in "subregionA" within "regionA" and has a population of "populationA".
-                                The capital of "CountryName A" is "capitalA".
-                                {/* France is located in Western Europe within Europe and has a population of 66,990,000.
-                                The capital of France is Paris. */}
-                                </p>
-                            </div>        
-                            <div class="w-full md:w-4/12 px-4 mr-auto ml-auto mt-24" data-aos="flip-left" data-aos-delay="1000">
-                                <div class="relative flex flex-col min-w-0 bg-gray-300 w-full shadow-lg rounded-lg">
-                                    <img
-                                        alt="Flag of first country"
-                                        src={FlagA}
-                                        class="w-full align-middle rounded-t-lg rounded-b-lg"
-                                    />
+                        {currencyFrom !== '' && currencyFrom !== 'X' && 
+                            currencyTo !== '' && currencyTo !== 'X' && amountIn !== '' 
+                        ? <div>
+                            <div className="flex flex-wrap items-center ">  
+                                <div className="flex flex-wrap items-center mt-24">
+
+                                    <div className="w-full md:w-8/12 px-4 mr-auto ml-auto  text-center" data-aos="zoom-in-up" data-aos-delay="0">
+                                        {/* <div className="text-gray-600 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-gray-100">
+                                        <i className="fas fa-user-friends text-xl"></i>
+                                        </div> */}
+                                        <h3 className="text-3xl mb-2 font-semibold leading-normal">
+                                        Your Currency Conversion                                
+                                        </h3>
+                                        <p className="text-lg font-light leading-relaxed mt-4 mb-4 text-gray-700">
+                                        "AmountIn" "from currency" from "CountryName A" equals  
+                                        "AmountOut" "To currency" from "CountryName B". 
+                                        {/* £20 British Pound Sterling from the United Kingdom equals €21.85 Euro from France.                                 */}
+                                        </p>
+                                    </div>
+                                </div>     
+                                <div className="w-full md:w-4/12 px-4 mr-auto ml-auto mt-24" data-aos="zoom-in-right">
+                                    <h3 className="text-3xl mb-2 font-semibold leading-normal">
+                                    "Country A"
+                                    {/* France */}
+                                    </h3>
+                                    <p className="text-lg font-light leading-relaxed mt-4 mb-4 text-gray-700">
+                                    "CountryName A" uses "from currency" as currency and speaks "languageA". 
+                                    {/* France uses Euro as currency and speaks French. */}
+                                    </p>
+                                    <p className="text-lg font-light leading-relaxed mt-0 mb-4 text-gray-700">
+                                    "CountryName A" is located in "subregionA" within "regionA" and has a population of "populationA".
+                                    The capital of "CountryName A" is "capitalA".
+                                    {/* France is located in Western Europe within Europe and has a population of 66,990,000.
+                                    The capital of France is Paris. */}
+                                    </p>
+                                </div>        
+                                <div className="w-full md:w-4/12 px-4 mr-auto ml-auto mt-24" data-aos="flip-left" data-aos-delay="1000">
+                                    <div className="relative flex flex-col min-w-0 bg-gray-300 w-full shadow-lg rounded-lg">
+                                        <img
+                                            alt="Flag of first country"
+                                            src={FlagA}
+                                            className="w-full align-middle rounded-t-lg rounded-b-lg"
+                                        />
+                                    </div>
                                 </div>
                             </div>
-                        </div>
 
-                        <div class="flex flex-wrap items-center">
-                            <div class="w-full md:w-4/12 px-4 mr-auto ml-auto mt-24" data-aos="flip-right" data-aos-delay="1000">
-                                <div class="relative flex flex-col min-w-0 bg-gray-300 w-full  shadow-lg rounded-lg">
-                                    <img
-                                        alt="Flag of second country"
-                                        src={FlagB}
-                                        class="w-full align-middle rounded-t-lg rounded-b-lg"
-                                    />
+                            <div className="flex flex-wrap items-center">
+                                <div className="w-full md:w-4/12 px-4 mr-auto ml-auto mt-24" data-aos="flip-right" data-aos-delay="1000">
+                                    <div className="relative flex flex-col min-w-0 bg-gray-300 w-full  shadow-lg rounded-lg">
+                                        <img
+                                            alt="Flag of second country"
+                                            src={FlagB}
+                                            className="w-full align-middle rounded-t-lg rounded-b-lg"
+                                        />
+                                    </div>
                                 </div>
+                                <div className="w-full md:w-4/12 px-4 mr-auto ml-auto mt-24" data-aos="zoom-in-left" data-aos-delay="0">
+                                    <h3 className="text-3xl mb-2 font-semibold leading-normal">
+                                    "Country B"
+                                    </h3>
+                                    <p className="text-lg font-light leading-relaxed mt-4 mb-4 text-gray-700">
+                                    "CountryName B" uses "to currency" as currency and speaks "languageB". 
+                                    </p>
+                                    <p className="text-lg font-light leading-relaxed mt-0 mb-4 text-gray-700">
+                                    "CountryName B" is located in "subregionB" within "regionB" and has a population of "populationB".
+                                    The capital of "CountryName B" is "capitalB".
+                                    </p>
+                                </div>    
                             </div>
-                            <div class="w-full md:w-4/12 px-4 mr-auto ml-auto mt-24" data-aos="zoom-in-left" data-aos-delay="0">
-                                <h3 class="text-3xl mb-2 font-semibold leading-normal">
-                                "Country B"
-                                </h3>
-                                <p class="text-lg font-light leading-relaxed mt-4 mb-4 text-gray-700">
-                                "CountryName B" uses "to currency" as currency and speaks "languageB". 
-                                </p>
-                                <p class="text-lg font-light leading-relaxed mt-0 mb-4 text-gray-700">
-                                "CountryName B" is located in "subregionB" within "regionB" and has a population of "populationB".
-                                The capital of "CountryName B" is "capitalB".
-                                </p>
-                            </div>    
                         </div>
+                        : null
+                        }
+
+
 
                     </div> 
 
                     <div className="relative pt-32 pb-32 flex content-center items-center justify-center mt-40">
                         <div className="absolute w-full h-full bg-center bg-cover">
-                            <video fluid={false} width={"100%"} height={"100%"} overflow-hidden loop muted autoPlay
+                            <video  width={"100%"} height={"100%"} overflow-hidden="true" loop muted autoPlay
                                 style={{objectFit: "cover"}}>
-                                <source fluid={false}  src={Money} type="video/mp4" />
+                                <source fluid="false"  src={Money} type="video/mp4" />
                             </video>
                         </div>
                     </div>
@@ -321,4 +344,4 @@ x                    <span id="blackOverlay" className="w-full h-full absolute o
     }
 }
 
-export default Landing;
+export default App;
