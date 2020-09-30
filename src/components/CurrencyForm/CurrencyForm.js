474055e15d46b5b4ee8f1f1ac538cc43b6edc143
amym321 <66137093+amym321@users.noreply.github.com>
2020-09-29 20:58:53 -0500

    removed options list

--- src/components/CurrencyForm/CurrencyForm.js
+++ src/components/CurrencyForm/CurrencyForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
+import { options } from './CurrencyOptions';
 import Dropdown from 'react-dropdown';
-// import CurrencyOptions from './CurrencyOptions3';
 
 
 class CurrencyForm extends React.Component {
@@ -12,6 +12,7 @@ class CurrencyForm extends React.Component {
             currency: '',
             amountIn: 0,
             amountOut: 0,
+            options: options,
             selected: ''
         }
         this._onSelectFrom = this._onSelectFrom.bind(this)
@@ -79,55 +80,8 @@ class CurrencyForm extends React.Component {
 
     render() {
         const { amountIn, selected, currencyFrom, currencyTo, countryA, countryB, amountOut, currency, onInputChange2 } = this.props;
-        const options = [
-            { value: 'USD', label: 'United States Dollar' },
-            { value: 'AUD', label: 'Australian Dollar' },
-            { value: 'BRL', label: 'Brazilian Real' },
-            { value: 'GBP', label: 'British Pound Sterling' },
-            { value: 'BLG', label: 'Bulgarian Lev' },
-            { value: 'CAD', label: 'Canadian Dollar' },
-            { value: 'CLP', label: 'Chilean Peso' },
-            { value: 'CNY', label: 'Chinese Yuan' },            
-            { value: 'COP', label: 'Colombian Peso' },
-            { value: 'CZK', label: 'Czech Republic Koruna' },
-            { value: 'DKK', label: 'Danish Krone' },
-            { value: 'DOP', label: 'Dominican Peso' },
-            { value: 'EGP', label: 'Egyptian Pound' },
-            { value: 'EUR', label: 'Euro (used in France)' },
-            { value: 'EUR', label: 'Euro (used in Germany)' },
-            { value: 'EUR', label: 'Euro (used in Austria)' },
-            { value: 'EUR', label: 'Euro (used in Belgium)' },
-            { value: 'EUR', label: 'Euro (used in Greece)' },
-            { value: 'EUR', label: 'Euro (used in Ireland)' },
-            { value: 'EUR', label: 'Euro (used in Italy)' },
-            { value: 'EUR', label: 'Euro (used in Netherlands)' },
-            { value: 'EUR', label: 'Euro (used in Portugal)' },
-            { value: 'EUR', label: 'Euro (used in Spain)' },    
-            { value: 'GTQ', label: 'Guatemalan Quetzal' },
-            { value: 'HKD', label: 'Hong Kong Dollar' },
-            { value: 'HUF', label: 'Hungarian Forint' },
-            { value: 'IDR', label: 'Indonesian Rupiah' },
-            { value: 'ILS', label: 'Israeli New Sheqel"' },
-            { value: 'INR', label: 'Indian Rupee' },
-            { value: 'ISK', label: 'Icelandic Króna' },
-            { value: 'JPY', label: 'Japanese Yen' },
-            { value: 'KRW', label: 'South Korean Won' },
-            { value: 'MYR', label: 'Malaysian Ringgit"' },      
-            { value: 'MXN', label: 'Mexican Peso' },
-            { value: 'NOK', label: 'Norwegian Krone' },
-            { value: 'NZD', label: 'New Zealand Dollar' },
-            { value: 'PHP', label: 'Philippine Peso' },
-            { value: 'PLN', label: 'Polish Zloty' },
-            { value: 'RUB', label: 'Russian Ruble' },
-            { value: 'SAR', label: 'Saudi Riyal' },
-            { value: 'SEK', label: 'Swedish Krona' },
-            { value: 'CHF', label: 'Swiss Franc' },
-            { value: 'THB', label: 'Thai Baht' },
-            { value: 'TRY', label: 'Turkish Lira' },
-            { value: 'ZAR', label: 'South African Rand' },
-            { value: 'AED', label: 'United Arab Emirates Dirham' } 
-        ]
         // const defaultOption = options[0];
+        const { options } = this.state
         const defaultOption = this.state.selected
         const placeHolderValue = typeof  
         this.state.selected === 'string' 
@@ -166,7 +120,6 @@ class CurrencyForm extends React.Component {
                             </div>   
                             <div className="mt3">
                                 <label className="db fw6 lh-copy f6">Initial Currency</label>
-                                {/* <GetOptions optionChoices={optionChoices}/> */}
                                 <Dropdown 
                                     className="pa2 input-reset ba bg-transparent hover-bg-white w-100 black" 
                                     arrowClosed={arrowClosed}
